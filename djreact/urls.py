82c7dc05d1f89d86a502a1c30766b42e272b07d0
Yomen Tohmaz <yomen.tohmaz@gmail.com>
2021-06-13 21:15:41 -0400

    Trying to fix URL bug.

--- djreact/urls.py
+++ djreact/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
     path('', include('focus.urls')),
-    # url(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
+    url(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ]
