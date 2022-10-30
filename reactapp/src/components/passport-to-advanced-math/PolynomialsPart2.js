7b9e31a89f04be46211803ee181d1f136e79e1d5
YomenT <yomen.tohmaz@gmail.com>
2022-10-29 20:59:31 -0400

    Reverting Image tag.

--- reactapp/src/components/passport-to-advanced-math/PolynomialsPart2.js
+++ reactapp/src/components/passport-to-advanced-math/PolynomialsPart2.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from 'react-image-resizer'
 
 class PolynomialsPart2 extends React.Component {
     render() {
@@ -50,8 +49,8 @@ class PolynomialsPart2 extends React.Component {
                         </li>
                     </ol>
 
-                    <Image 
-                        img src={"https://i.ibb.co/q9M24cN/Adding-Polynomials2.png"}
+                    <img 
+                        src={"https://i.ibb.co/q9M24cN/Adding-Polynomials2.png"}
                         alt="Function Notation"
                         width={350}
                         height={350} />
