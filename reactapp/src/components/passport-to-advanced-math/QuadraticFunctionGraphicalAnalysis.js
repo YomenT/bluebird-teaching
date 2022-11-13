3536fd2f373c1bed1408ef0e109a92d7a7c62a68
YomenT <yomen.tohmaz@gmail.com>
2022-11-13 14:01:55 -0500

    Added explanation

--- reactapp/src/components/passport-to-advanced-math/QuadraticFunctionGraphicalAnalysis.js
+++ reactapp/src/components/passport-to-advanced-math/QuadraticFunctionGraphicalAnalysis.js
@@ -44,6 +44,17 @@ class QuadraticFunctionGraphicalAnalysis extends React.Component {
                     alt="Graphical Analysis 1"
                     width={350}
                     height={350} />
+
+                <p className="p-body">
+                    There is only one difference.  The one that had a positive x<sup>2</sup> opened upward, and the one with a negative x<sup>2</sup> opened downward.  Let's 
+                    break this down.
+                </p>
+
+                <p className="p-body-small">
+                    Remember the standard form we learned about in the last lesson (ax<sup>2</sup> + bx + c = 0)?  In the equations we used, there is no "bx", but that's not 
+                    needed; we'll get into its importance later.  Whether "bx" is there or not, if the "a" value is positive, the graph opens upward; if the "a" value is 
+                    negative, the graph opens downward.
+                </p>
             </div>
         )
     }
