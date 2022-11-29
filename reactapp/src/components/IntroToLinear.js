81561590982cda4b5711d0744da379f62bd534c7
YomenT <yomen.tohmaz@gmail.com>
2022-11-28 21:58:14 -0500

    Attempting to fix IntroToLinear fraction

--- reactapp/src/components/IntroToLinear.js
+++ reactapp/src/components/IntroToLinear.js
@@ -20,10 +20,11 @@ class IntroToLinear extends React.Component {
                     <li>The <i>slope</i> measures the steepness of a line.  It is also known as the rate of change.  Slope is also often defined 
                     as <sup>rise</sup>&frasl;<sub>run</sub>.</li>
                     <li>Below is the equation to find slope given two points.</li>
-                    <div class="frac" style={{ fontSize: "17px", marginLeft: "47%" }}>
-                        <span>y<sub>2</sub> - y<sub>1</sub></span>
-                        <span class="symbol">/</span>
-                        <span class="bottom">x<sub>2</sub> - x<sub>1</sub></span>
+                    <div className='q'>
+                        <div class="frac" style={{ fontSize: "17px", marginLeft: "47%" }}>
+                            <div className='numer'>y<sub>2</sub> - y<sub>1</sub></div>
+                            <div className='denom'>x<sub>2</sub> - x<sub>1</sub></div>
+                        </div>
                     </div>
                     <li>Like any other number, a slope can be positive or negative.</li>
                 </ul>
