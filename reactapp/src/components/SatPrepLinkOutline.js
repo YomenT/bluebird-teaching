92e111d68969293b8681e44091e1042d08e56c31
YomenT <yomen.tohmaz@gmail.com>
2022-12-06 14:09:08 -0500

    Updated links

--- reactapp/src/components/SatPrepLinkOutline.js
+++ reactapp/src/components/SatPrepLinkOutline.js
@@ -5,8 +5,49 @@ class SatPrepLinkOutline extends React.Component {
         return (
             <div>
                 <h4 className="h4-blue-headers">Heart of Algebra</h4>
+                <ol className="p-body">
+                    <li>A little bit about Heart of Algebra</li>
+                    <li>Introduction to Functions</li>
+                    <li>Function Notation</li>
+                    <li>Introduction to Linear Functions</li>
+                    <li>Modelling Linear Equations</li>
+                    <li>Paralell and Perpendicular Lines</li>
+                    <li>System of Linear Equations</li>
+                        <ul className="p-body">
+                            <li>Part 1</li>
+                            <li>Part 2</li>
+                        </ul>
+                    <li>Linear Inequalities</li>
+                    <li>Graphing Linear Inequalities</li>
+                    <li>Graphing Systems of Linear Inequalities</li>
+                </ol>
                 <h4 className="h4-blue-headers">Problem Solving and Data Analysis</h4>
                 <h4 className="h4-blue-headers">Passport to Advanced Math</h4>
+                <ol className="p-body">
+                    <li>A little bit about Passport to Advanced Math</li>
+                    <li>Polynomials</li>
+                        <ul className="p-body">
+                            <li>Part 1</li>
+                            <li>Part 2</li>
+                            <li>Part 3</li>
+                        </ul>
+                    <li>Introduction to the Quadratic Function</li>
+                    <li>Quadratic Function Graphical Analysis</li>
+                        <ul className="p-body">
+                            <li>Part 1</li>
+                            <li>Part 2</li>
+                        </ul>
+                    <li>Factoring Quadratic Equations</li>
+                        <ul className="p-body">
+                            <li>Part 1</li>
+                            <li>Part 2</li>
+                        </ul>
+                    <li>Exponent Rules</li>
+                        <ul className="p-body">
+                            <li>Part 1</li>
+                            <li>Part 2</li>
+                        </ul>
+                </ol>
                 <h4 className="h4-blue-headers">Additional Topics in Math</h4>
             </div>
         )
