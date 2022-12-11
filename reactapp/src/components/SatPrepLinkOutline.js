d3981091bcaef1d041b680079aba6ace28c659dc
YomenT <yomen.tohmaz@gmail.com>
2022-12-11 00:58:44 -0500

    testing JS

--- reactapp/src/components/SatPrepLinkOutline.js
+++ reactapp/src/components/SatPrepLinkOutline.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom"
 
 class SatPrepLinkOutline extends React.Component {
+
+    visitedLink(id) {
+        document.getElementById(id).style.color = "#121d2d";
+    }
+
     render() {
         return (
             <div>
                 <h4 className="h4-blue-headers">Heart of Algebra</h4>
                 <ol className="list-medium">
-                    <Link to="/heart_of_algebra/about" style={{ color: "#365789" }}><li style={{ paddingTop: "20px" }}><strong>A little bit about Heart of Algebra</strong></li></Link>
+                    <Link to="/heart_of_algebra/about" style={{ color: "#365789" }}><li style={{ paddingTop: "20px" }} id='link1' onClick={visitedLink(this.id)}><strong>A little bit about Heart of Algebra</strong></li></Link>
                     <Link to="/heart_of_algebra/introduction_to_functions" style={{ color: "#365789" }}><li>Introduction to Functions</li></Link>
                     <Link to="/heart_of_algebra/function_notation" style={{ color: "#365789" }}><li>Function Notation</li></Link>
                     <Link to="/heart_of_algebra/introduction_to_linear" style={{ color: "#365789" }}><li>Introduction to Linear Functions</li></Link>
