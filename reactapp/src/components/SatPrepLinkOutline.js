bd4f61bf2427ad4472206a5891258ca080813402
YomenT <yomen.tohmaz@gmail.com>
2022-12-17 23:53:42 -0500

    JS Testing

--- reactapp/src/components/SatPrepLinkOutline.js
+++ reactapp/src/components/SatPrepLinkOutline.js
@@ -19,9 +19,9 @@ class SatPrepLinkOutline extends React.Component {
     render() {
         return (
             <div style={{ paddingBotton: "25px" }}>
-                <h4 className="h4-blue-headers" onMouseOver={this.changeLinkColor.bind}>Heart of Algebra</h4>
+                <h4 className="h4-blue-headers">Heart of Algebra</h4>
                 <ol className="list-medium">
-                    <li style={{ paddingTop: "20px" }}><Link to="/heart_of_algebra/about" style={{ color: "#365789" }}><strong>A little bit about Heart of Algebra</strong></Link></li>
+                    <li style={{ paddingTop: "20px" }} onMouseOver={this.changeLinkColor.bind}><Link to="/heart_of_algebra/about" style={{ color: "#365789" }}><strong>A little bit about Heart of Algebra</strong></Link></li>
                     <li><Link to="/heart_of_algebra/introduction_to_functions" style={{ color: "#365789" }}>Introduction to Functions</Link></li>
                     <li><Link to="/heart_of_algebra/function_notation" style={{ color: "#365789" }}>Function Notation</Link></li>
                     <Link to="/heart_of_algebra/introduction_to_linear" style={{ color: "#365789" }}><li>Introduction to Linear Functions</li></Link>
