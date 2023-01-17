8e9ded3e5a064048505fbe00eab451772b0fc3c3
YomenT <yomen.tohmaz@gmail.com>
2023-01-17 18:10:19 -0500

    Finished introduction for quadratics assessment.

--- reactapp/src/components/passport-to-advanced-math/QuadraticEquationsFoundationTest.js
+++ reactapp/src/components/passport-to-advanced-math/QuadraticEquationsFoundationTest.js
@@ -21,7 +21,19 @@ class QuadraticEquationsFoundationTest extends React.Component {
                         in factoring, the quadratic formula, and completing the square in various other applications.
                     </p>
 
+                    <p className="p-body">
+                        Before moving on, it's important to really make sure we have the foundations of quadratics 100% understood.  
+                        Below are a series of questions that'll hopefully ensure that understanding of quadratics is ready to go.
+                    </p>
 
+                    <p className="p-body-small">
+                        Remember that this isn't a test or quiz.  I'm obviously not going to grade any of this.  I'm not even going to 
+                        see whether you got anything right or wrong.  It's really all up to you.  After you've answered the questions, do 
+                        you feel like there are still any questions in your head?  Something doesn't seem right?  If there's any doubt at 
+                        all, please feel free to reach out!  We're happy to answer any questions, or just discuss quadratics; sometimes 
+                        discussing a topic is really the best practice there is.  Ever wondered how quadratics are used in the real world?  
+                        Feel free to discuss that with us, or look that up on your own.  Either way, don't hesitate to "talk" quadratics.
+                    </p>
                 </div>
                 <Footer />
             </div>
