87826dc0e6ecce3e382c7c7263ea79edd9bee4c4
YomenT <yomen.tohmaz@gmail.com>
2023-01-19 23:26:01 -0500

    Fixing scrolling problem

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import App from './App'
+import ScrollToTop from './components/ScrollToTop'
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
 <BrowserRouter>
+    <ScrollToTop />
     <App /> 
 </BrowserRouter>, 
 document.getElementById('root')
