66fbacc28e168e01f6dc4e189492eccf4b94ecc8
YomenT <yomen.tohmaz@gmail.com>
2023-02-12 10:38:38 -0500

    Added to rational expressions part 1.

--- reactapp/src/components/passport-to-advanced-math/RationalExpressions.js
+++ reactapp/src/components/passport-to-advanced-math/RationalExpressions.js
@@ -41,22 +41,48 @@ class RationalExpressions extends React.Component {
 
                     <div className="row" style={{ paddingTop: "50px" }}>
                         <div className="col-4">
-                        <img 
-                            src={"https://i.ibb.co/zXXNZTy/Rational-Expressions1.jpg"}
-                            alt="Adding rational expressions example."
-                            className="mx-auto d-block"
-                            width={300}
-                            height={250} />
+                            <img 
+                                src={"https://i.ibb.co/zXXNZTy/Rational-Expressions1.jpg"}
+                                alt="Adding rational expressions example."
+                                className="mx-auto d-block"
+                                width={300}
+                                height={250} />
                         </div>
 
                         <div className="col-8">
-                            <p className="p-body-small align-middle">
+                            <p className="p-body-small">
                                 In the first line, we have the original problem; we're trying to add those to fractions (also known as rational expressions).  In the 
                                 second line, to the left of the arrow, I am multiplying the top and bottom of <sup>7</sup>&frasl;<sub>x</sub> by 5.  As the right of 
                                 the arrow shows, I now have common denominators.  On the third and last line, following the normal steps of adding fractions, I add 
                                 the numerators to get the final answer 
                             </p>
                         </div>
+                    </div>
+
+                    <div className="row" style={{ paddingTop: "50px" }}>
+                        <div className="col-4">
+                            <img 
+                                src={"https://i.ibb.co/BcG12Tm/Rational-Expressions2.jpg"}
+                                alt="Adding rational expressions example."
+                                className="mx-auto d-block"
+                                width={300}
+                                height={250} />
+                        </div>
+
+                        <dic className="col-8">
+                            <p className="p-body-small">
+                                Again, we'll go line by line.  The first line just contains the original problem; we're trying to add those two rational expressions.  
+                                The second line is where I'm trying to get common denominators.  I multiply the top and bottom of the left fraction by (x - 2), and I 
+                                multiply the top and bottom of the right fraction with (x + 2).  The third line shows what everything looks like at this point; you'll 
+                                see that both fractions now have common denominators.  In the fourth line; to the left of the equal sign, I have FOILed the numerators 
+                                of both fractions, and now I can go about normally adding fractions.
+                            </p>
+
+                            <p className="p-body-small">
+                                Now this is an example where there would be a bit more to do if we were also simplifying, but we're just worrying about adding right now.  
+                                We'll learn about simplifying rational expressions in a later lesson.
+                            </p>
+                        </dic>
                     </div>
                     
                 </div>
