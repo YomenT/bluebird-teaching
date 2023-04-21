2d9491b57c737f79fc6188d5bac869de7fd0ef5f
YomenT <yomen.tohmaz@gmail.com>
2023-04-21 02:38:26 -0400

    Completed QuadraticInequalities

--- reactapp/src/components/passport-to-advanced-math/QuadraticInequalities.js
+++ reactapp/src/components/passport-to-advanced-math/QuadraticInequalities.js
@@ -46,6 +46,31 @@ class QuadraticInequalities extends React.Component {
                         />
                     </div>
 
+                    <p className="p-body-small">
+                        All this stuff about utilizing the number line shown in the video, and finding x values that fulfill the inequality might sound new.  If all this 
+                        is confusing, <i>please don't hesitate to reach out to us with any questions</i>.
+                    </p>
+
+                    <h4 className="h4-blue-headers" style={{ paddingTop: "75px" }}>Try it!</h4>
+
+                    <p className="p-body-small">
+                        I don't know if you noticed, but the video above is divided into 10 parts.  We've only went over the "Problem #1" part.  For practice, try 
+                        going through the rest of the 10 problems.  In all cases, you're still going to be solving quadratic inequalities.  I highly recommend attempting 
+                        each problem first before watching the solution part.  When the initial problem is displayed at the beginning of each part, please pause the 
+                        video and attempt the problem given.
+                    </p>
+
+                    <h4 className="h4-blue-headers" style={{ paddingTop: "75px" }}>Create it!</h4>
+
+                    <p className="p-body">
+                        Create a simple diagram that shows the differences between solving a quadratic equation and solving a quadratic inequality.  Try doing a step by 
+                        step demonstration for each kind.  You can even use the same quadratic; where one version is an equation, and the other version is an inequality.  
+                    </p>
+
+                    <p className="p-body-small">
+                        As a bonus, try also showing the differences/similarities when factoring a quadratic equation versus an inequality (if there are any differences).
+                    </p>
+
                 </div>
 
             <Footer />
