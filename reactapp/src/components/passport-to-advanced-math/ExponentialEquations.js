9a9e542a1a3cfa475ef5bbd4ac1503cfb6844b77
YomenT <yomen.tohmaz@gmail.com>
2023-04-22 11:15:19 -0400

    Added to ExponentialEquations lesson.

--- reactapp/src/components/passport-to-advanced-math/ExponentialEquations.js
+++ reactapp/src/components/passport-to-advanced-math/ExponentialEquations.js
@@ -38,6 +38,21 @@ class ExponentialEquations extends React.Component {
                         <span style={{ color: "#5b92e5" }}><i>b<sup>m</sup> = b<sup>n</sup> &rarr; m = n</i></span>
                     </p>
 
+                    <p className="p-body-medium">
+                        Now if we look at a practical example...
+                    </p>
+
+                    <p className="p-body-medium" style={{ textAlign: "center", paddingTop: "50px", paddingBottom: "50px" }}>
+                        <span style={{ color: "#5b92e5" }}>2<sup>x</sup> = 2<sup>x - 5</sup> &rarr; 2 = x - 5 &rarr; x = 7</span>
+                    </p>
+
+                    <p className="p-body-medium">
+                        Seems easy enough, right?  The only catch is that sometimes the SAT will have a problem where the bases don't look the same, but you have to actually 
+                        get them to be the same.  For example, <i>2<sup>3</sup></i> and <i>4<sup>x</sup></i> don't initially have the same bases, but if we apply our power 
+                        rule in reverse, we can get them to be the same bases.  Isn't <i>4</i> the same as <i>2<sup>2</sup></i>?  With that, we just substitute it to look 
+                        like <i>(2<sup>2</sup>)<sup>x</sup></i>; now we have the same bases.
+                    </p>
+
                 </div>
             
             <Footer />
