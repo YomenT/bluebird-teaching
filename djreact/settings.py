17981db5350dc800b337d9c0f6bea4e72171acfc
YomenT <yomen.tohmaz@gmail.com>
2023-07-27 16:59:06 +0000

    Commiting changes for envrionment variables.

--- djreact/settings.py
+++ djreact/settings.py
@@ -93,6 +93,13 @@ TEMPLATES = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'yomen.tohmaz.edu@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+
 CORS_ALLOWED_ORIGINS = [
     "https://bluebird-teaching.web.app",
     "https://bluebird-teaching.com",
