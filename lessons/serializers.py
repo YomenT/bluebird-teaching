88c5cf3a46ea2bd969772117d687987f6b434ca2
khansameer25 <realskhan25@gmail.com>
2023-08-07 15:17:32 -0400

    Added completed field to lessons

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -4,4 +4,4 @@ from .models import Lesson
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = ('id', 'title', 'subset_name', 'introduction', 'try_it', 'create_it')
+        fields = ('id', 'title', 'subset_name', 'introduction', 'try_it', 'create_it', 'completed')
