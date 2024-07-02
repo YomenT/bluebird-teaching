30e92cec32f41eb17ca403a26cc2b546543282af
Yomen Tohmaz <yomen.tohmaz@gmail.com>
2024-07-02 21:52:49 +0400

    Committing Changes

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from adminsortable2.admin import SortableAdminMixin
-from .models import Subset, Lesson
+from .models import Subset, Lesson, Comment
 
 class LessonAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ('title', 'subset_name', 'order') 
 
 admin.site.register(Subset)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Comment)
