60057e58c428c2c4313de4fb333e9745838adff0
Yomen Tohmaz <yomen.tohmaz@gmail.com>
2024-09-11 20:26:09 +0400

    Committing changes

--- djreact/urls.py
+++ djreact/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(documentation.urls)),
+    path('', include('documentation.urls')),
     path('', include('focus.urls')),
     path('', include('lessons.urls')),
     path('', include('send-email.urls')),
